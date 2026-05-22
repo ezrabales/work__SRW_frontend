377a76f5d811a60c98d47f4d6b9e61f39b159e0a
ezrabales <bales.ezra@gmail.com>
2026-05-22 08:57:21 -0600

    add vercel.json file

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <h1 className="about__title">About</h1>
       <p className="about__description">
         Solid Rock Websites equips the Church with modern, mobile friendly, and
-        visually engaging website that strengthen community and expand outreach.
+        visually engaging websites that strengthen community and expand outreach.
       </p>
       <p className="about__description">
         I love Jesus and I love coding. I want to combine my skills and desires
